e23ef259ee56891cc7d27014638eede7691782f7
liam humphreys <liam.humphreys1989@gmail.com>
2019-06-27 10:47:26 +0100

    fix root node tests

--- src/nodes/root-node.spec.js
+++ src/nodes/root-node.spec.js
@@ -15,19 +15,48 @@ import kastree.ast.psi.Parser
 import kastree.ast.psi.Converter
 import java.io.File
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
-
-
-class ColorHelpFormatter : CliktHelpFormatter() {
-    private val tc = TermColors(TermColors.Level.ANSI16)
-
-    override fun renderTag(tag: String, value: String) = tc.green(super.renderTag(tag, value))
-    override fun renderOptionName(name: String) = tc.yellow(super.renderOptionName(name))
-    override fun renderArgumentName(name: String) = tc.yellow(super.renderArgumentName(name))
-    override fun renderSubcommandName(name: String) = tc.yellow(super.renderSubcommandName(name))
-    override fun renderSectionTitle(title: String) = (tc.bold + tc.underline)(super.renderSectionTitle(title))
-    override fun optionMetavar(option: HelpFormatter.ParameterHelp.Option) = tc.green(super.optionMetavar(option))
-}`
+import com.google.gson.GsonBuilder as gsonBuilder
+import com.google.gson.reflect.*`
     ).toMatchFormat());
+
+  test("with extra lines", () =>
+    expect(
+      `package com.github.angrypotato.kotato
+
+
+  import com.github.ajalt.clikt.core.CliktCommand
+  import com.github.ajalt.clikt.core.NoRunCliktCommand
+  import com.github.ajalt.clikt.core.context
+  import com.github.ajalt.clikt.core.subcommands
+
+
+
+  import com.github.ajalt.clikt.output.CliktHelpFormatter
+  import com.github.ajalt.clikt.output.HelpFormatter
+  import com.github.ajalt.clikt.parameters.arguments.argument
+
+  import com.github.ajalt.mordant.TermColors
+  import kastree.ast.psi.Parser
+  import kastree.ast.psi.Converter
+  import java.io.File
+
+  import com.google.gson.Gson
+  import com.google.gson.GsonBuilder as gsonBuilder
+  import com.google.gson.reflect.*`
+    ).toChangeFormat(`package com.github.angrypotato.kotato
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.output.CliktHelpFormatter
+import com.github.ajalt.clikt.output.HelpFormatter
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.mordant.TermColors
+import kastree.ast.psi.Parser
+import kastree.ast.psi.Converter
+import java.io.File
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder as gsonBuilder
+import com.google.gson.reflect.*`));
 });
