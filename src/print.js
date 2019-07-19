b65f9a79d8b985cd4d76e762392610d43ac07984
liam humphreys <liam.humphreys1989@gmail.com>
2019-07-19 11:38:35 +0100

    rm refactor-friendly control flow

--- src/print.js
+++ src/print.js
@@ -10,10 +10,6 @@ module.exports = (path, opts, print) => {
   const typedNode = assignType(node);
 
   if (typedNode.astType) {
-    if (nodes[typedNode.astType].print) {
-      return nodes[typedNode.astType].print(path, opts, print);
-    } else {
-      return nodes[typedNode.astType](path, opts, print);
-    }
+    return nodes[typedNode.astType].print(path, opts, print);
   }
 };
